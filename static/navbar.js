039f0b2b1efaf110df4196423c50d7c477257e17
hinuiiik <github@vknc.mozmail.com>
2024-11-10 14:22:43 -0500

    Initial.

---
+++ static/navbar.js
@@ -0,0 +1,8 @@
+function navToggle() {
+    let x = document.getElementById("topnavbar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
