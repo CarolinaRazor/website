ad28a9909cb22cd61e8c2f44a092961fde056cbf
Vikram <hello@vikramk.dev>
2025-11-26 15:36:48 -0500

    Fix allowing user 1 to give themselves admin

--- src/collections/Users/hooks/protectRoles.ts
+++ src/collections/Users/hooks/protectRoles.ts
@@ -3,17 +3,12 @@ import type {User} from '@/payload-types'
 
 export const protectRoles: FieldHook<{id: string} & User> = ({req, data}) => {
   const isAdmin = req.user?.roles?.includes('admin')
-  const id = req.user?.id
-  // console.log("id: " + id)
 
-  if (!isAdmin) {
+  if (!isAdmin && req.user?.id !== 1) {
     return ['user']
   }
 
   const userRoles = new Set(data?.roles || [])
   userRoles.add('user')
-  if (req.user?.id === 1) {
-    userRoles.add('admin')
-  }
   return [...userRoles.values()]
 }
